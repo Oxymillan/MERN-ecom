5ed7c79d7abc9fbd240543196665922ab1eae58e
JinEnomura <oxymje@gmail.com>
2020-06-19 22:31:49 +0100

    Single product fetch to db rendered and route defined

--- amazona/frontend/src/Screens/HomeScreen.js
+++ amazona/frontend/src/Screens/HomeScreen.js
@@ -1,7 +1,7 @@
 import React, { useEffect } from "react";
 import { Link } from "react-router-dom";
 import { useSelector, useDispatch } from "react-redux";
-import { listProducts } from "../actions/productActions";
+import { listProducts } from "../actions/productActions.js";
 
 function HomeScreen(props) {
   // define hook
--- amazona/frontend/src/store.js
+++ amazona/frontend/src/store.js
@@ -1,12 +1,13 @@
 import { createStore, combineReducers, applyMiddleware, compose } from 'redux';
 import thunk from 'redux-thunk';
-import { productListReducer } from './reducers/productReducers.js';
+import { productListReducer, productDetailsReducer } from './reducers/productReducers.js';
 
 const initialState = {};
 const reducer = combineReducers({
   productList: productListReducer,
+  productDetails: productDetailsReducer
 })
 
 const composeEnhanser = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
-const store = createStore(reducer, initialState, compose(applyMiddleware(thunk)));
+const store = createStore(reducer, initialState, composeEnhanser(applyMiddleware(thunk)));
 export default store;
