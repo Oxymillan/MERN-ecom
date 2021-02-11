e56e0b48e6a696684b9797dee4df7df780134df2
oxy <oxy@localhost.localdomain>
2021-02-11 15:00:49 +0000

    bump axios

--- backend/config.js
+++ backend/config.js
@@ -7,6 +7,6 @@ export default {
   MONGODB_URL: process.env.MONGODB_URL || 'mongodb://localhost/shop',
   JWT_SECRET: process.env.JWT_SECRET || 'somethingsecret',
   PAYPAL_CLIENT_ID: process.env.PAYPAL_CLIENT_ID || 'sb',
-  accessKeyId: process.env.accessKeyId || 'accessKeyId',
-  secretAccessKey: process.env.secretAccessKey || 'secretAccessKey',
+  accessKeyId: process.env.ACCESS_KEY_ID || 'accessKeyId',
+  secretAccessKey: process.env.SECRET_ACCESS_KEY || 'secretAccessKey',
 };
